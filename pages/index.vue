1de0b02dbe55fa4b2cc76e3750e5868bb96db868
Fernando Obieta <fernando.obieta@blanktree.ch>
2026-08-04 12:43:39 +0200

    small updates on exhibitions

--- pages/index.vue
+++ pages/index.vue
@@ -13,15 +13,15 @@
 						<b>currently / upcoming</b>
 					</p>
 					<ul>
-            <li>"messages" exhibition and talk – SHIFTING REGIMES, CHANGING ORDERS – 25.–26.06.2026, Goethe University Frankfurt/Main</li>
+            <li>"messages" exhibition and talk "on machines congregating" – SHIFTING REGIMES, CHANGING ORDERS – 25.–26.06.2026, Goethe University Frankfurt/Main</li>
             <li>«zu Zombiewissen» – contribution to the publication «Exemplar #2» – June 2026 – <a href="https://www.hslu.ch/de-ch/design-film-kunst/aktuelles/veroeffentlichungen/exemplar/" target="_blank">HSLU DFK Exemplar Reihe</a></li>
-            <li>group exhibition "Visarte Zürich – Einsichten" – 29.08.–12.09.2026, Kupper Modern, Zwinglistrasse 10, 8004 Zürich</li>
+            <li>no Artificial Intelligence – group exhibition "Visarte Zürich – Einsichten" – 29.08.–12.09.2026, Kupper Modern, Zwinglistrasse 10, 8004 Zürich</li>
             <li>machines congregating – excerpts #6+#7 – 06.09.2026, 17:00–19:00, Lagerplatz 18, 8400 Winterthur</li>
-            <li>group exhibition "knowledge beyond" – 15.–29.10.2026, HSLU DFK, Nylsuisseplatz 1, 6020 Luzern</li>
-            <li>duo exhibition "Visarte Zürich – FRISCH" – 29.10.–01.11.2026, Kabinett Visarte, Schoffelgasse 10, 8001 Zürich</li>
+            <li>i dropped the metal ball on the glass table and it broke. what is it? & messages – group exhibition "knowledge beyond" – 15.–29.10.2026, HSLU DFK, Nylsuisseplatz 1, 6020 Luzern</li>
+            <li>roaming & messages – duo exhibition "Visarte Zürich – FRISCH" – 29.10.–01.11.2026, Kabinett Visarte, Schoffelgasse 10, 8001 Zürich</li>
             <li>keynote, GLAMhack 2026 – 12.11.2026 – Campo Winterthur</li>
             <li>machines congregating – excerpt #8 – 15.11.2026, 17:00–19:00, Lagerplatz 18, 8400 Winterthur</li>
-            <li>solo exhibition – 29.04.–27.06.2027, Benzeholz, Benzeholzplatz 2, 6045 Meggen</li>
+            <li>on communicating / on communication – solo exhibition – 29.04.–27.06.2027, Benzeholz, Benzeholzplatz 2, 6045 Meggen</li>
 					</ul>
 				</div>
 			</div>
